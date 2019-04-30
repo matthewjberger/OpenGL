0df34a6cb6e62f2b2364f06a7a27af60c17174e6
Matthew J. Berger <matthewberger@nevada.unr.edu>
2019-04-30 14:58:13 -0700

    Update source to include opengl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use glfw::{Action, Context, Key};
+
 fn main() {
-    println!("Hello, world!");
+    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+
+    let (mut window, events) = glfw
+        .create_window(
+            300,
+            300,
+            "Hello this is a window",
+            glfw::WindowMode::Windowed,
+        )
+        .expect("Failed to create GLFW window.");
+
+    window.set_key_polling(true);
+    window.make_current();
+
+    gl::load_with(|s| glfw.get_proc_address_raw(s));
+
+    while !window.should_close() {
+        glfw.poll_events();
+        for (_, event) in glfw::flush_messages(&events) {
+            handle_window_event(&mut window, event);
+        }
+
+        unsafe {
+            gl::ClearColor(0.2, 0.3, 0.3, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+    }
+}
+
+fn handle_window_event(window: &mut glfw::Window, event: glfw::WindowEvent) {
+    match event {
+        glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => window.set_should_close(true),
+        _ => {}
+    }
 }
